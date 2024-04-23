7be078d461088a37a36979c35eecf27715abbeb6
Natashawilson4 <128700377+Natashawilson4@users.noreply.github.com>
2024-04-23 16:06:09 +0100

    Update svm.m

--- svm.m
+++ svm.m
@@ -1,18 +1,1 @@
-Train = readtable("clean_train.csv");
-Xtrain = table2array(Train(:, 3:end-1));
-Ytrain = categorical(Train.satisfaction);
 
-% Load testing data
-Test = readtable("clean_test.csv");
-Xtest = table2array(Test(:, 3:end-1));
-Ytest = categorical(Test.satisfaction);
-
-% Train SVM model
-SVMModel = fitcsvm(Xtrain, Ytrain, 'Standardize', true, 'KernelFunction', 'linear');
-
-% Predict using the trained model
-Ypred = predict(SVMModel, Xtest);
-
-% Evaluate model performance
-accuracy = sum(Ypred == Ytest) / numel(Ytest);
-fprintf('Accuracy of SVM model: %.2f%%\n', accuracy * 100);
